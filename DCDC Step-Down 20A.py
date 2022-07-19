afbae1ce02c8a5fdc2ae7957cc2e3de04f7272bf
Paul Zech <paul@zechs.de>
2022-07-19 12:35:39 +0200

    First two plots created

---
+++ DCDC Step-Down 20A.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_excel('DCDC Step-Down 20A.xlsx')
+print (df)
+
+x_1 = df.iloc[1:4, 9]
+x_2 = df.iloc[5:8, 9]
+x_3 = df.iloc[9:12, 9]
+x_4 = df.iloc[13:16, 9]
+print(x_4)
+
+y_1 = df.iloc[1:4, 11]
+y_2 = df.iloc[5:8, 11]
+y_3 = df.iloc[9:12, 11]
+y_4 = df.iloc[13:16, 11]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot(x_2, y_2, marker='^')
+plt.plot(x_3, y_3, marker='s')
+plt.plot(x_4, y_4, marker='o')
+
+plt.xlim([0, 175])
+plt.ylim([91, 97])
+plt.xlabel('Ausgangsleistung [W]')
+plt.ylabel('Wirkungsgrad [%]')
+plt.legend(['Ausgangsspannung 10 V', 'Ausgangsspannung 12 V','Ausgangsspannung 15 V', 'Ausgangsspannung 20 V'], loc='upper right')
+
+plt.show()
---
+++ DCDC Step-Up 40A.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_excel('DCDC Step-Up 40A.xlsx')
+print (df)
+
+x_1 = df.iloc[1:4, 10]
+x_2 = df.iloc[5:8, 10]
+x_3 = df.iloc[9:11, 10]
+x_4 = df.iloc[12, 10]
+print(x_4)
+
+y_1 = df.iloc[1:4, 13]
+y_2 = df.iloc[5:8, 13]
+y_3 = df.iloc[9:11, 13]
+y_4 = df.iloc[12, 13]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot(x_2, y_2, marker='^')
+plt.plot(x_3, y_3, marker='s')
+plt.plot(x_4, y_4, marker='o')
+
+plt.xlim([20, 175])
+plt.ylim([88, 95])
+plt.xlabel('Ausgangsleistung [W]')
+plt.ylabel('Wirkungsgrad [%]')
+plt.legend(['Ausgangsspannung 15 V', 'Ausgangsspannung 20 V','Ausgangsspannung 25 V', 'Ausgangsspannung 30 V'], loc='upper right')
+
+plt.show()
