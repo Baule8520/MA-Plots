6034fe8502e06a33ad9f1e617386a3ed459b7c16
Paul Z <paul@zechs.de>
2023-04-21 13:35:53 +0200

    Added ADS ADC & 0.05 Ohm shunt

--- Shunt/Readme.md
+++ Shunt/Readme.md
@@ -5,6 +5,7 @@
 I measured how accurate Shunt current measurement is...
 
 - 0.1 Ohm shunt with Arduino ADC
+- 0.05 Ohm shunt with ADS1115 ADC & Arduino
 
 ## Circuit Diagrams
 
@@ -15,6 +16,7 @@ Will follow...
 ## Software
 
 - [Shunt](https://github.com/PaulusElektrus/Uno_Shunt) --> 0.1 Ohm shunt
+- [ADS1115](https://github.com/PaulusElektrus/Arduino_and_ADS1115) --> 0.05 Ohm shunt
 
 ## Plots
 
@@ -22,6 +24,9 @@ Can be found under /results.
 
 ## Findings
 
-- The 0,1 Ohm shunt results with the arduino are acceptable, but with 3 A shunt is getting warm and results are getting bad...
+- The 0,1 Ohm shunt results with the arduino are acceptable, but with 3 A shunt is getting warm (3 A * 3 A * 0.1 Ohm = 0.9 Watt) and results are getting bad...
 - Also there is some unlinearity
 - To compare with Hall Sensor see [here](https://github.com/PaulusElektrus/MA-Plots/tree/main/Hall_Sensor/results) --> plot file ends with _shunt.svg
+
+- The 0.05 Ohm shunt in combination with ADS1115 works perfectly fine and with 3 A (3 A * 3 A * 0.05 Ohm = 0.45 Watt) the shunt is not getting warm. 4 A results in 0.8 Watt shunt is getting warm.
+- Results are very good with maximal 50 mA deviation
---
+++ Shunt/shunt_ads1115.py
@@ -0,0 +1,111 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_excel('data/ADS1115_Shunt.xlsx')
+
+x_1 = df.iloc[2:26, 0]
+print(x_1)
+
+y_1 = df.iloc[2:26, 3]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot([0,4], [0,4], linestyle='--', color='black')
+
+plt.xlabel('Referenzstrom [A]')
+plt.ylabel('gemessener Strom [A]')
+plt.legend(['Messwert', "Idealwert"], loc='lower right')
+
+plt.savefig('results/shunt_ads.svg', format='svg', dpi=1200)
+plt.clf()
+
+############################################################################################################
+
+y_1 = df.iloc[2:26, 6]
+
+x_1 = df.iloc[2:26, 0]
+
+x = [0,4]
+y_oben = [0.0346791, 0.0346791]
+y_unten = [-0.0346791, -0.0346791]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot([0,4], [0,0], linestyle='--', color='black')
+plt.fill_between(x, y_oben, color='green', alpha=.5)
+plt.fill_between(x, y_unten, color='green', alpha=.5)
+
+plt.ylim(-2,2)
+plt.xlabel('Strom [A]')
+plt.ylabel('Abweichung [%]')
+plt.legend(['Messwert',"Idealwert"], loc='lower right')
+
+plt.savefig('results/shunt_ads_prozent.svg', format='svg', dpi=1200)
+plt.clf()
+
+############################################################################################################
+
+y_1 = df.iloc[2:26, 4]
+
+x_1 = df.iloc[2:26, 0]
+
+x = [0,4]
+y_oben = [1.387163636, 1.387163636]
+y_unten = [-1.387163636, -1.387163636]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot([0,4], [0,0], linestyle='--', color='black')
+plt.fill_between(x, y_oben, color='green', alpha=.5)
+plt.fill_between(x, y_unten, color='green', alpha=.5)
+
+plt.ylim(-100,100)
+plt.xlabel('Strom [A]')
+plt.ylabel('Abweichung [mA]')
+plt.legend(['Messwert',"Idealwert"], loc='lower right')
+
+plt.savefig('results/shunt_ads_ma.svg', format='svg', dpi=1200)
+plt.clf()
+
+############################################################################################################
+
+y_1 = df.iloc[2:26, 6]
+
+x_1 = df.iloc[2:26, 0]
+
+x = [0,4]
+y_oben = [0.0346791, 0.0346791]
+y_unten = [-0.0346791, -0.0346791]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot([0,4], [0,0], linestyle='--', color='black')
+plt.fill_between(x, y_oben, color='green', alpha=.5)
+plt.fill_between(x, y_unten, color='green', alpha=.5)
+
+plt.ylim(-6,6)
+plt.xlabel('Strom [A]')
+plt.ylabel('Abweichung [%]')
+plt.legend(['Messwert',"Idealwert"], loc='lower right')
+
+plt.savefig('results/shunt_ads_prozent_vg.svg', format='svg', dpi=1200)
+plt.clf()
+
+############################################################################################################
+
+y_1 = df.iloc[2:26, 4]
+
+x_1 = df.iloc[2:26, 0]
+
+x = [0,4]
+y_oben = [1.387163636, 1.387163636]
+y_unten = [-1.387163636, -1.387163636]
+
+plt.plot(x_1, y_1, marker='x')
+plt.plot([0,4], [0,0], linestyle='--', color='black')
+plt.fill_between(x, y_oben, color='green', alpha=.5)
+plt.fill_between(x, y_unten, color='green', alpha=.5)
+
+plt.ylim(-250,250)
+plt.xlabel('Strom [A]')
+plt.ylabel('Abweichung [mA]')
+plt.legend(['Messwert',"Idealwert"], loc='lower right')
+
+plt.savefig('results/shunt_ads_ma_vg.svg', format='svg', dpi=1200)
+plt.clf()
